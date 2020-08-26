2ef7572d093afd4426639c13e7616cc81fcb55cf
Uiyun Kim <66242193+uiyunkim-git@users.noreply.github.com>
2020-08-26 20:53:47 +0900

    update

--- src/database.py
+++ src/database.py
@@ -1,40 +1,19 @@
 import pymysql
 
-db = pymysql.connect(host='sql313.epizy.com',
-                     port=3306,
-                     user='epiz_26589997',
-                     passwd='EwZ25pT5vjS08',
-                     db='epiz_26589997_XXX',
-                     charset='utf8')
+def runsql(statement,param):
+    db = pymysql.connect(host='hackathon.c41ospthg6le.us-east-2.rds.amazonaws.com',
+                         port=3306,
+                         user='umlkorean',
+                         passwd='#wQy6IQY6v10Bp91cG^s8yJU!',
+                         db='RAN',
+                         charset='utf8')
 
+    cursor = db.cursor()
 
-cursor = db.cursor()
+    sql = statement
 
-sql = """
-    select * from (
-           id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-           name VARCHAR(20) NOT NULL,
-           model_num VARCHAR(10) NOT NULL,
-           model_type VARCHAR(10) NOT NULL,
-           PRIMARY KEY(id)
-    );
-    """
+    cursor.execute(sql,param)
+    cursor.execute("SHOW TABLES")
 
-# sql = """
-#     CREATE TABLE korea (
-#            id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-#            name VARCHAR(20) NOT NULL,
-#            model_num VARCHAR(10) NOT NULL,
-#            model_type VARCHAR(10) NOT NULL,
-#            PRIMARY KEY(id)
-#     );
-#     """
-
-cursor.execute(sql)
-cursor.execute("SHOW TABLES")
-
-
-db.commit()
-
-
-db.close()
+    db.commit()
+    db.close()
--- src/parser.py
+++ src/parser.py
@@ -2,12 +2,33 @@ from pyresparser import ResumeParser
 import os
 import json
 from src.environment import DATA_PATH
-
+from src.database import runsql
 
 resume_path = os.path.join(DATA_PATH,"resume")
+
+id = 0
 
 for file in os.listdir(resume_path):
     filePath = os.path.join(resume_path,file)
     data = ResumeParser(filePath,skills_file=os.path.join(DATA_PATH,"skills.csv")).get_extracted_data()
 
-    print(json.dumps(data["skills"]))
+    statement = """insert into student values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+
+    values = list(data.values())
+    values.insert(0,id)
+
+    tmp = []
+    for val in values:
+        if isinstance(val,list):
+            tmp.append(json.dumps(val))
+        else:
+            tmp.append(val)
+
+
+    print(tuple(tmp))
+
+    for each in tuple(values):
+        print(type(each),end='')
+
+    runsql(statement,tuple(tmp))
+    id += 1
