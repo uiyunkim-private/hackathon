235d77f2d4fa2972ec06d25a40c4e4819884186a
Uiyun Kim <66242193+uiyunkim-git@users.noreply.github.com>
2020-08-28 14:06:55 +0900

    updated predictor

--- src/google_word_similarity/predictor.py
+++ src/google_word_similarity/predictor.py
@@ -1,4 +1,8 @@
 import gensim.downloader as api
 
 model = api.load("word2vec-google-news-300")
-print(model.most_similar(positive=["creative","technology"], negative=[]))
+
+
+
+
+print(model.most_similar(positive=["artificial","intelligence"], negative=[]))
